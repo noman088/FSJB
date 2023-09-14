d6c0294f051478802dee45927f16923957757cac
Noman <mdnoman70499@gmail.com>
2023-09-14 14:31:16 +0530

    learn something in Js

---
+++ JavaScript/10_11Dec/Sat_Script.js
@@ -0,0 +1,65 @@
+// // // <------Primitive Data Types or Primitive Values-------->
+
+// // 7;
+// // 2.5;
+// // // Number Values
+
+// // ("Noman");
+// // ("Noman");
+// // // String Values
+
+// // true;
+// // false;
+// // // Boolean Values
+
+// // null;
+// // undefined;
+// // // Empty Values
+
+// // // All these above are Primitive Values or Primitive Data type
+// // // Primtive Data types=> One single Value;
+
+// // // <-----Array and Object--------->
+
+// // [1, 2, "Noman", true, "anurag", "Shivam", "noman", "akram"];
+// // //0,1      2       3      4          5          6       7
+
+// // // Object
+
+// // {
+// //   // Key:values
+// //   fname: Noman;
+// //   lname: Ahmad;
+// // }
+// // // Non -Primitive Data types =>Will more than one value
+
+// // <-----Variables=>Place Holder for value---->
+// // let x = 24;
+// // const z = '3434255232';//constant
+// // const balance = 9999999;
+
+// let firstname = "Noman";
+// let lastname = "Ahmad";
+// const mobileno = "123455673";
+// // console.log(lastname);
+// // console.log(mobileno);
+// // console.log(firstname);
+
+// // console.log("My first Name is ", firstname);
+// // console.log("My last Name is " + lastname);
+
+// // those both are same no difference
+
+// // console.log(mobileno,firstname,lastname);
+
+// // <======= Template Literals ====>
+// console.log(`My Current first Name is ${firstname}  ${lastname} ${mobileno}`);
+
+// // // What we understood till now
+// // Basic Pillar of an programming language;
+// //     Values;
+// // Data types;
+// // Difference between promitive and non primitive Datatypes
+// // Console.log => How to print something on terminal
+// // Declaring Variable
+// // Tempalate Literal
