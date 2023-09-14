d3c36c054ad46072ed2c2beda5ef6e5a621b67f8
Noman <mdnoman70499@gmail.com>
2023-09-14 14:55:32 +0530

    compelete piller 1 Values operations and Variables

--- JavaScript/10_11Dec/Sat_Script.js
+++ JavaScript/10_11Dec/Sat_Script.js
@@ -1,65 +1,105 @@
-// // // <------Primitive Data Types or Primitive Values-------->
+// // <------Primitive Data Types or Primitive Values-------->
+/*
+// 7;
+// 2.5;
+// // Number Values
 
-// // 7;
-// // 2.5;
-// // // Number Values
+// ("Noman");
+// ("Noman");
+// // String Values
 
-// // ("Noman");
-// // ("Noman");
-// // // String Values
+// true;
+// false;
+// // Boolean Values
 
-// // true;
-// // false;
-// // // Boolean Values
+// null;
+// undefined;
+// // Empty Values
 
-// // null;
-// // undefined;
-// // // Empty Values
+// // All these above are Primitive Values or Primitive Data type
+// // Primtive Data types=> One single Value;
 
-// // // All these above are Primitive Values or Primitive Data type
-// // // Primtive Data types=> One single Value;
+*/
 
-// // // <-----Array and Object--------->
+/*
+// // <-----Array and Object--------->
 
-// // [1, 2, "Noman", true, "anurag", "Shivam", "noman", "akram"];
-// // //0,1      2       3      4          5          6       7
+// [1, 2, "Noman", true, "anurag", "Shivam", "noman", "akram"];
+// //0,1      2       3      4          5          6       7
 
-// // // Object
+// // Object
 
-// // {
-// //   // Key:values
-// //   fname: Noman;
-// //   lname: Ahmad;
-// // }
-// // // Non -Primitive Data types =>Will more than one value
+// {
+//   // Key:values
+//   fname: Noman;
+//   lname: Ahmad;
+// }
+// // Non -Primitive Data types =>Will more than one value
 
-// // <-----Variables=>Place Holder for value---->
-// // let x = 24;
-// // const z = '3434255232';//constant
-// // const balance = 9999999;
+// <-----Variables=>Place Holder for value---->
+// let x = 24;
+// const z = '3434255232';//constant
+// const balance = 9999999;
 
-// let firstname = "Noman";
-// let lastname = "Ahmad";
-// const mobileno = "123455673";
-// // console.log(lastname);
-// // console.log(mobileno);
-// // console.log(firstname);
+let firstname = "Noman";
+let lastname = "Ahmad";
+const mobileno = "123455673";
+// console.log(lastname);
+// console.log(mobileno);
+// console.log(firstname);
 
-// // console.log("My first Name is ", firstname);
-// // console.log("My last Name is " + lastname);
+// console.log("My first Name is ", firstname);
+// console.log("My last Name is " + lastname);
 
-// // those both are same no difference
+// those both are same no difference
 
-// // console.log(mobileno,firstname,lastname);
+// console.log(mobileno,firstname,lastname);
 
-// // <======= Template Literals ====>
-// console.log(`My Current first Name is ${firstname}  ${lastname} ${mobileno}`);
+// <======= Template Literals ====>
+console.log(`My Current first Name is ${firstname}  ${lastname} ${mobileno}`);
 
-// // // What we understood till now
-// // Basic Pillar of an programming language;
-// //     Values;
-// // Data types;
-// // Difference between promitive and non primitive Datatypes
-// // Console.log => How to print something on terminal
-// // Declaring Variable
-// // Tempalate Literal
+// // What we understood till now
+// Basic Pillar of an programming language;
+//     Values;
+// Data types;
+// Difference between promitive and non primitive Datatypes
+// Console.log => How to print something on terminal
+// Declaring Variable
+// Tempalate literals
+
+
+*/
+
+/*
+
+// < ----- Operator---------->
+
+// // Assignment Operator;
+// let x = 10;
+// let y = 10;
+
+// // Arithmetic Operator
+// // + , - , * , / , %
+// let z = x + y;
+// console.log(z);
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x % y);
+
+// // Comparison Operator
+
+// let abc = 11;
+// let xyz = '11';
+// // console.log(abc > xyz);
+// // console.log(abc < xyz);
+// // console.log(abc >= xyz);
+
+// // == or ===
+// console.log(abc==xyz);// Compare only value not data type
+// console.log(abc===xyz);// Compare value as well as data type
+
+*/
+
+
