a51ac4794ce4d6e3bf69018174480e85100fc8ed
Noman <mdnoman70499@gmail.com>
2023-11-19 04:44:59 +0530

    just en exp of Hof other will do later

---
+++ JavaScript/28 Jan 2023/HOF.js
@@ -0,0 +1,38 @@
+// HOF --> Higher Order Function
+// Callback
+// function is calling a function
+
+//  Higher Order functions are functions which take other function as a parameter or return a function as a value. The function passed as a parameter is called callback.
+
+// function a() {
+//   console.log("I was called by a function ");
+// }
+
+// function b() {
+//   a();
+// }
+
+// b();
+// <--------------------------------------------->
+//Mini Program
+
+// const callback = (n) => {
+//   return n ** 2;
+// };
+
+// function cube(callback,n) {
+//   return callback(n) * n;
+// }
+
+// console.log(cube(callback,n));
+
+// <------------------------------------------->
+
+// For each
+const arr = ["hey", "hii", "hola", "Namaste"];
+
+// arr.forEach()
+arr.forEach(myfunc);
+function myfunc(val) {
+  console.log(val);
+}
