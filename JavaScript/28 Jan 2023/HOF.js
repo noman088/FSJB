df1996ec33946a5a123416d6258928a33b49c957
Noman <mdnoman70499@gmail.com>
2023-11-19 16:43:30 +0530

    Destructuring done

--- JavaScript/28 Jan 2023/HOF.js
+++ JavaScript/28 Jan 2023/HOF.js
@@ -97,7 +97,6 @@
 //  If we do not specify this parameter, by default accumulator will get array first value.
 //  If our array is an empty array, then Javascript will throw an erro
 
-
 // const num = [1, 2, 3, 4, 5, 6, 7];
 // const sum = num.reduce((acc, curr) => acc + curr, 1);
 // console.log(sum);
@@ -118,3 +117,17 @@
 
 // console.log(names.reverse());// Reverse names by alphabetically
 
+// <----Destrcturing and Spread and Rest----------->
+
+// const sci = [2.72, 3.14, 9.81, 37, 100];
+// let [e, pi, grav, bodytemp, boiltemp] = sci;
+// console.log(e, pi, grav, bodytemp, boiltemp);
+
+// const arr1 = [1, 2, 3];
+// let [var1, , var2] = arr1;
+// console.log(var1, var2);
+
+const arr2 = [1, 2, 3, 4, 5, 6, 7, 8];
+let [num1, num2, ...rest] = arr2;
+console.log(num1, num2);
+console.log(rest);
