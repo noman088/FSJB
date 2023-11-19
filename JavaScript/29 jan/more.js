b149847331c4dadd9b662592a8b787da15f144ad
Noman <mdnoman70499@gmail.com>
2023-11-19 17:40:35 +0530

    spread and rest done

---
+++ JavaScript/29 jan/more.js
@@ -0,0 +1,28 @@
+// HOF and Callback
+// Rst and spread
+// try and Catch
+//  Promises and  at the end  a small MCQ's Games
+
+// Spread and rest ...
+// function sumone(x, y) {
+//   return x + y;
+// }
+// let vari = [5, 7];
+
+// console.log(sumone(...vari)); // Spread
+
+// <-----Rest------>
+
+// function sum(...args) {
+//   // Rest Operator
+//   //   console.log(args); //tell what  is it  in the args
+//   let sum = 0;
+//   for (const x of args) {
+//     sum = sum + x;
+//   }
+//   return sum;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 0));
+
+
