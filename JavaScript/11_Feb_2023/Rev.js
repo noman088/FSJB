1678421cf28299793c1df2914905410fca675386
Noman <mdnoman70499@gmail.com>
2023-11-29 05:35:39 +0530

    Map , Filter and Reduce Done

---
+++ JavaScript/11_Feb_2023/Rev.js
@@ -0,0 +1,20 @@
+// Agenda->
+// Map , Filter and Reduce
+// Promise and fetch API
+// Wheather App
+
+// map=> iterate over aray and perform a action
+// filter=>iterate over array and filter out the element
+// reduce => single value
+
+// const number = [1, 2, 3, 4, 5];
+// const num = number.map((vab) => vab * 5);
+// console.log(num);
+
+// const count = ["india", "Finland", "Shrilanka", "Ireland"];
+// const country = count.filter((val) => val.includes("land"));
+// console.log(country);
+
+const num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const sum = num1.reduce((acc, curr) => acc + curr, 0);
+console.log("Sum Of All Element is ", sum);
