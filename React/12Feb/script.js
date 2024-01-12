1ddae07c963d2380d1902861a53155679f2e7090
Noman <mdnoman70499@gmail.com>
2024-01-12 23:51:48 +0530

    react start

---
+++ React/12Feb/script.js
@@ -0,0 +1,17 @@
+// const name = <h1>Anurag Tiwari</h1>;
+// JSX Element
+// const name=<p>Noman</p> // JSX Element
+
+// Element can contain only one element . so that all in header 
+// const header =
+//   <header>
+//     <h1> Hello </h1>
+//     <p>Hello Namaste</p>
+//   </header>
+
+// const header = <>
+//     <h1>Hello </h1>
+//     <p>Hello Namaste </p>
+
+// </>// React fragment ... if tag doesn't have any name 
+
